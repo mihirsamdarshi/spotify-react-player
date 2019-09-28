caeeb97c89d45e8bfea2bec9553e0b9324bcd25c
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-09-27 18:49:51 -0700

    formatting updates

--- src/GiphyImage.jsx
+++ src/GiphyImage.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import './GiphyImage.css'
+import './GiphyImage.css';
 
 class GiphyImage extends Component {
   render() {
-    const { image } = this.props
+    const { image } = this.props;
     return (
       <div className="GiphyImage">
         <img src={image.images.fixed_width.url} alt={image.source_tld} />
       </div>
-    )
+    );
   }
 }
 
-export default GiphyImage
+export default GiphyImage;
---
+++ src/SearchForm.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import './SearchForm.css';
+
+import SearchResults from './SearchResults';
+
+import { searchGifs } from './api';
+
+class SearchForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: null,
+            query: '',
+            images: [],
+        };
+    }
+
+
+handleChange = (event) => this.setState({
+    [event.target.name]: event.target.value,
+})
+
+performQuery = (event) => {
+    event.preventDefault();
+
+    this.setState({
+        error: null,
+    });
+
+    searchGifs({
+        rating: 'pg-13',
+        q: this.state.query,
+    }).then((result) => this.setState({
+        images: result.data,
+    })).catch(() => this.setState({
+        error: 'Sorry, but something went wrong.',
+    }));
+}
+
+render() {
+    return (
+        <form className="SearchForm" onSubmit={this.performQuery}>
+            <p>Enter a search term:</p>
+
+            <input name="query" type="text" value={this.state.query} onChange={this.handleChange} />
+
+            <div className="ButtonBar">
+                <button type="submit" disabled={!this.state.query}>Search Giphy!</button>
+            </div>
+
+            {this.state.error && (
+                <div className="error">
+                    {this.state.error}
+                </div>
+            )}
+
+            <SearchResults results={this.state.images} />
+        </form>
+    );
+}
+}
+
+export default SearchForm;
