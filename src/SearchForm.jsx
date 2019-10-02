30714276cdfb34df95cae4459e7c9b71c6e84db4
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-10-01 20:39:38 -0700

    remove extraneous files, formatting

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -17,57 +17,45 @@ class SearchForm extends Component {
         };
     }
 
-    handleChange = (event) => this.setState({
-        [event.target.name]: event.target.value,
-    })
+  handleChange = (event) => this.setState({
+      [event.target.name]: event.target.value,
+  })
 
-    performQuery = (event) => {
-        event.preventDefault();
+  performQuery = (event) => {
+      event.preventDefault();
 
-        this.setState({ error: null });
+      this.setState({
+          error: null,
+      });
 
-        searchGifs({
-            rating: 'pg-13',
-            q: this.state.query,
-        })
-            .then((result) => this.setState({ images: result.data }))
-            .catch(() => this.setState({ error: 'Sorry, but something went wrong.' }));
-    }
+      searchGifs({
+          rating: 'pg-13',
+          q: this.state.query,
+      }).then((result) => this.setState({
+          images: result.data,
+      })).catch(() => this.setState({
+          error: 'Sorry, but something went wrong.',
+      }));
+  }
 
-    render() {
-        return (
-            <form className="SearchForm" onSubmit={this.performQuery}>
-                <p>
-                    {' '}
-                Enter a search term:
-                    {' '}
-                    {' '}
-                    {' '}
-                </p>
-                <input name="query" type="text" value={this.state.query} onChange={this.handleChange} />
-                <div className="ButtonBar">
-                    <button type="submit" disabled={!this.state.query}>
-                        {' '}
-                    Search Giphy!
-                        {' '}
-                        {' '}
-                        {' '}
-                    </button>
-                </div>
-                { this.state.error && (
-                    <div className="error">
-                        {' '}
-                        {this.state.error}
-                        {' '}
-                    </div>
-                )}
-                <SearchResults results={this.state.images} />
-                {' '}
-                {' '}
-                {' '}
-            </form>
-        );
-    }
+  render() {
+      return (
+          <form className="SearchForm" onSubmit={this.performQuery}>
+              <p>Enter a search term:</p>
+              <input name="query" type="text" value={this.state.query} onChange={this.handleChange} />
+              <div className="ButtonBar">
+                  <button type="submit" disabled={!this.state.query}>Search Giphy!</button>
+              </div>
+              {this.state.error && (
+                  <div className="error">
+                      {this.state.error}
+                  </div>
+              )}
+
+              <SearchResults results={this.state.images} />
+          </form>
+      );
+  }
 }
 
 export default SearchForm;
