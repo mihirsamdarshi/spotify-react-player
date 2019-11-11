4dca8e3af7baa206a4c37b0fb5b0522a21f10667
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-11-11 14:31:51 -0800

    add basic login info

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import Hero from './Hero';
 import MainAppBody from './MainAppBody';
 
 import { apiHost } from '../scripts/api';
+import { SetLoginDispatch } from '../scripts/callbacks';
 
 const App = () => {
     // Because App is the "uppermost" component (see index.js), code in the useEffect function
@@ -13,14 +14,17 @@ const App = () => {
     // can have its own useEffect, and so initialization can be separated on a per-component
     // basis.
     useEffect(() => apiHost('http://api.giphy.com/v1/'));
-    const [loggedIn, setClientLoggedIn] = useState(false);
+    const {accessToken, setAccessToken} = useState(null);
 
     // When React components are implemented as functions, their return value is the component’s
     // content (i.e., what the render() method returns for class-based components).
+
     return (
         <div className="App">
             <AppHeader />
-            {loggedIn ? <MainAppBody /> : <Hero login={setClientLoggedIn} />}
+            <SetLoginDispatch.Provider value={setAccessToken}>
+                {accessToken ? <MainAppBody /> : <Hero />}
+            </SetLoginDispatch.Provider>
         </div>
     );
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import '../stylesheets/Hero.scss';
 
-const Hero = (props) => (
-    <div className="fillWindow">
-        <div className="heroInfo">
-            <h1> Applotify </h1>
-            <h3> Play Spotify Playlists&#42; </h3>
-        </div>
-        <div className="centeredBtn">
-            <button type="submit" className="spotifyLoginBtn" onClick={() => props.login(true)}>
-                Login With Spotify
-            </button>
-        </div>
+import { SetLoginDispatch } from '../scripts/callbacks';
 
-        <p className="asterisk"> &#42; only available in Chrome and Firefox browsers due to Spotify API limitations</p>
-    </div>
-);
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = "ccc1ea71746a4d4ea8468a7cc7d1d9f0";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+    "user-read-private",
+    "user-modify-playback-state",
+    "streaming",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
+
+const Hero = props => {
+    const getHashParams = () => {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+            q = window.location.hash.substring(1);
+        e = r.exec(q)
+        while (e) {
+            hashParams[e[1]] = decodeURIComponent(e[2]);
+            e = r.exec(q);
+        }
+        return hashParams;
+    };
+
+    const setAccessToken = useContext(SetLoginDispatch);
+
+    useEffect(() => {
+        () => setAccessToken(getHashParams());
+    }, []);
+
+    return (
+        <div className="fillWindow">
+            <div className="heroInfo">
+                <h1> Applotify </h1>
+                <h3> Play Spotify Playlists&#42; </h3>
+            </div>
+            <div className="centeredBtn">
+                <a
+                    className="spotifyLoginBtn"
+                    href={`${authEndpoint}?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&show_dialog=true`}
+                >
+                    Login With Spotify
+                </a>
+            </div>
+
+            <p className="asterisk"> &#42; only available in Chrome and Firefox browsers and to users with Spotify Premium due to Spotify API limitations</p>
+        </div>
+    )
+};
 
 export default Hero;
