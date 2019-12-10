ea4f0d216317cdaa3e4770ef340c431d4a168d17
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-12-10 00:39:18 -0800

    figure out how to get element position

--- src/components/ScrollWheel.js
+++ src/components/ScrollWheel.js
@@ -1,25 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../stylesheets/ScrollWheel.scss';
 
 const ScrollWheel = props => {
 
     const [dragImage, setDragImage] = useState(null);
+    const elementPosition = useRef(null);
+
+    const getSize = element => {
+        if (element) {
+            this.props.getSize(element.getBoundingClientRect());
+        }
+    };
 
     const handleDrag = (event) => {
         event.dataTransfer.setDragImage(new Image(), 0, 0);
-        console.log(event);
+
+        elementPosition.current.focus();
+
+        const elementData = elementPosition.current.getBoundingClientRect()
+        const dragXPos = event.pageX;
+        const dragYPos = event.pageY;
+
+        //TODO: use the above logged function to calculate the position of the mouse in the component
+        //TODO: terminate dragevent if it leaves the circle
     };
 
     useEffect(() => {
         document.addEventListener('dragstart', handleDrag);
 
         return () => {
-            document.removeEventListener('dragend', handleDrag);
+            document.removeEventListener('dragstart', handleDrag);
         }
     });
 
     return (
-        <div className="container">
+        <div className="container" ref={elementPosition}>
             <div className="circle" draggable onDrag={(event) => handleDrag(event)} >
                 <div className="middleButton">
                     <div className="arrowContainer">
