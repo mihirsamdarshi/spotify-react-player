c7d23009d49d58ca93a95b55d597a62ccc5e2c56
Mihir Samdarshi <mihirsamdarshi@yahoo.com>
2019-12-11 17:40:02 -0800

    returning right position

--- src/components/ScrollWheel.js
+++ src/components/ScrollWheel.js
@@ -1,33 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import '../stylesheets/ScrollWheel.scss';
 
 const ScrollWheel = props => {
-    const [transform, setTransform] = useState('');
     const [angle, setAngle] = useState(0);
     const elementPosition = useRef(null);
 
+    const handleRotate = (event) => {
+        if (event.type === "mousedown") {
+            document.addEventListener('mousemove', handleRotate);
+            elementPosition.current.focus();
+            const elementData = elementPosition.current.getBoundingClientRect();
+            const xPos = event.clientX;
+            const yPos = event.clientY;
 
-    const handleDragStart = (event) => {
-        elementPosition.current.focus();
-        const elementData = elementPosition.current.getBoundingClientRect();
-        const xPos = event.clientX;
-        const yPos = event.clientY;
-
-        event.preventDefault();
-
-        console.log(getCenter(elementData));
-
-        const angle = getAngle(xPos, yPos, getCenter(elementData));
-
-        setAngle(angle);
+            const angle = getAngle(xPos, yPos, getCenter(elementData));
+            setAngle(angle);
+        } else {
+            document.removeEventListener('mousemove', handleRotate);
+        }
     };
 
     const getAngle = (xPos, yPos, center) => {
+
         const mouse = {
             x: xPos,
             y: yPos,
         };
-        console.log(mouse);
+
         return (Math.atan2(mouse.y - center.y, mouse.x - center.x) * (180 / Math.PI)) % 360;
     };
 
@@ -38,22 +37,10 @@ const ScrollWheel = props => {
         };
     };
 
-    useEffect(() => {
-        document.addEventListener('mousedown', handleDragStart);
-        document.addEventListener('mousemove', handleDragStart);
-        document.addEventListener('mouseup', handleDragStart);
-
-        return () => {
-            document.removeEventListener('mousedown', handleDragStart);
-            document.removeEventListener('mousemove', handleDragStart);
-            document.removeEventListener('mouseup', handleDragStart);
-        }
-    }, );
-
     return (
-        <div className="container" ref={elementPosition}>
-            <div className="circle">
-                <div className="circleContainer" draggable onDrag={(event) => handleDragStart(event)} style={{transform: [`rotate(${angle}deg)`]}}>
+        <div className="container" onMouseDown={handleRotate} onMouseUp={handleRotate}>
+            <div className="circle" ref={elementPosition}>
+                <div className="circleContainer" style={{transform: [`rotate(${angle}deg)`]}}>
                     <div className="grabbableCircle" />
                 </div>
                 <div className="middleButton" />
